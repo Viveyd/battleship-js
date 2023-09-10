9acb28f3fa0aa899118ee1d066a7d915ee5d1a60
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-09-10 16:03:44 +0800

    Ship() can return any of five ships

--- src/Ship.js
+++ src/Ship.js
@@ -1,5 +1,46 @@
-function Ship() {
-  return { };
+const shipList = {
+  carrier: Carrier,
+  battleship: Battleship,
+  destroyer: Destroyer,
+  submarine: Submarine,
+  'patrol boat': PatrolBoat,
+};
+
+function Ship(type) {
+  if (!type) return null;
+  const lcType = type.toLowerCase();
+  if (!shipList[lcType]) return null;
+  return { ...shipList[lcType]() };
+}
+
+function Carrier() {
+  return {
+    type: 'Carrier',
+  };
+}
+
+function Battleship() {
+  return {
+    type: 'Battleship',
+  };
+}
+
+function Destroyer() {
+  return {
+    type: 'Destroyer',
+  };
+}
+
+function Submarine() {
+  return {
+    type: 'Submarine',
+  };
+}
+
+function PatrolBoat() {
+  return {
+    type: 'Patrol Boat',
+  };
 }
 
 module.exports = Ship;
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -5,4 +5,13 @@ describe('Ship()', () => {
   it('returns an object', () => {
     assert.equal(typeof Ship(), 'object');
   });
+  it('returns an object of only 5 possible types', () => {
+    assert.equal(Ship('Carrier').type, 'Carrier');
+    assert.equal(Ship('Battleship').type, 'Battleship');
+    assert.equal(Ship('Destroyer').type, 'Destroyer');
+    assert.equal(Ship('Submarine').type, 'Submarine');
+    assert.equal(Ship('Patrol Boat').type, 'Patrol Boat');
+    assert.equal(Ship('SupaShip'), null);
+    assert.equal(Ship('Titanic'), null);
+  });
 });
